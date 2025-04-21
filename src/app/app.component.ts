651f81dab53c4ef92ec994e96225a95b8a0bc3b3
thaimeo200389 <thaimeo200389@gmail.com>
2025-04-21 07:07:46 +0000

    properly start/stop stream

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,40 +1,51 @@
-import { Component, ElementRef, ViewChild, AfterViewInit, OnDestroy } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  ViewChild,
+  AfterViewInit,
+  OnDestroy,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { WebRTCPlayer } from '@eyevinn/webrtc-player';
 
 @Component({
-    selector: 'app-root',
-    imports: [CommonModule, FormsModule],
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.css',
-    standalone: true
+  selector: 'app-root',
+  imports: [CommonModule, FormsModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+  standalone: true,
 })
 export class AppComponent implements AfterViewInit, OnDestroy {
-    whepUrl: string = '';
-    player: WebRTCPlayer | null = null;
-    @ViewChild('videoElement') videoElementRef!: ElementRef<HTMLVideoElement>;
+  whepUrl: string = '';
+  player: WebRTCPlayer | null = null;
+  @ViewChild('videoPlayer') videoElementRef!: ElementRef<HTMLVideoElement>;
 
-    ngAfterViewInit(): void {
-        const video = this.videoElementRef.nativeElement;
-        if (!video) {
-            console.error('Video element not found!');
-            return;
-        }
-        this.player = new WebRTCPlayer({
-            video: video,
-            type: 'whep',
-            statsTypeFilter: '^candidate-*|^inbound-rtp'
-        });
+  ngAfterViewInit(): void {
+    const video = this.videoElementRef?.nativeElement;
+    if (!video) {
+      console.error('Video element not found!');
+      return;
     }
-    play(): void {
-        if (this.player) {
-            this.player.load(new URL(this.whepUrl));
-        }
+    this.player = new WebRTCPlayer({
+      video: video,
+      type: 'whep',
+      statsTypeFilter: '^candidate-*|^inbound-rtp',
+    });
+  }
+  play(): void {
+    if (this.player) {
+      this.player.load(new URL(this.whepUrl));
     }
-    ngOnDestroy(): void {
-        if (this.player) {
-            this.player.destroy();
-        }
+  }
+  stop(): void {
+    if (this.player) {
+      this.player.unload();
     }
+  }
+  ngOnDestroy(): void {
+    if (this.player) {
+      this.player.destroy();
+    }
+  }
 }
